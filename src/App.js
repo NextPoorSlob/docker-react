11bc2c87a5436cd62d8d739e4b2b3a9607f58042
Stephen Gelman <sgelman@nextpoorslob.com>
2024-11-08 12:51:42 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hi, buddy! How you doing? I'm not good
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
